e14d4ab0f9f53b9cb8c599f5976a1beed8da617a
Joel <103879u@gmail.com>
2023-11-15 15:53:12 +0800

    consumer: write first GET /food test

---
+++ learn-pact-consumer/contracts/person.test.js
@@ -0,0 +1,40 @@
+import { pactWith } from "jest-pact";
+import { Matchers } from "@pact-foundation/pact";
+
+const getFoodService = (baseUrl) => {
+  return {
+    async getFood() {
+      return (await fetch(`${baseUrl}/food`)).json();
+    },
+  };
+};
+
+pactWith({ consumer: "Person", provider: "Food" }, (provider) => {
+  let client;
+
+  beforeEach(() => {
+    client = getFoodService(provider.mockService.baseUrl);
+  });
+
+  describe("food endpoint", () => {
+    beforeEach(() =>
+      provider.addInteraction({
+        state: "Food is available",
+        uponReceiving: "A request for food",
+        willRespondWith: {
+          status: 200,
+          body: {},
+        },
+        withRequest: {
+          method: "GET",
+          path: "/food",
+        },
+      })
+    );
+
+    it("returns status", async () => {
+      const body = await client.getFood();
+      expect(body).toEqual({});
+    });
+  });
+});
